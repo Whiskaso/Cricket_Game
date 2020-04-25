09ed4915ee5d50bbcf2c995195f5c5dbd2b80117
Whiskaso <marcinlakomy9@gmail.com>
2020-04-25 20:09:14 +0200

    add undo - troll button

--- script.js
+++ script.js
@@ -376,3 +376,21 @@ function actualization() {
 
   }, 300);
 }
+
+
+// print troll to undo button
+function printImage(source) {
+  var Pagelink = "about:blank";
+  var pwa = window.open(Pagelink, "_new");
+  pwa.document.open();
+  pwa.document.write(ImagetoPrint(source));
+  pwa.document.close();
+}
+
+function ImagetoPrint(source) {
+  return "<html><head><scri" + "pt>function step1(){\n" +
+    "setTimeout('step2()', 10);}\n" +
+    "function step2(){window.print();window.close()}\n" +
+    "</scri" + "pt></head><body onload='step1()'>\n" +
+    "<img src='" + source + "' /></body></html>";
+}
